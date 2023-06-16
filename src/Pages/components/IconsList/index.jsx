08053e277daabc1ffbb94c1e0cb23c13258b3ce7
5arSanti <santiari05@hotmail.com>
2023-06-16 10:57:43 -0500

    Change in the order of the icons.

--- src/Pages/components/IconsList/index.jsx
+++ src/Pages/components/IconsList/index.jsx
@@ -9,14 +9,14 @@ const IconsList = () => {
             <a target="_blank" rel="noreferrer" href="https://github.com/5arSanti">
                 <img src="https://cdn-icons-png.flaticon.com/512/733/733609.png"></img>
             </a>
-            <a target="_blank" rel="noreferrer" href="https://twitter.com/5ar_santi">
-                <img src="https://cdn-icons-png.flaticon.com/512/1384/1384017.png" alt="TwitterIcon"></img>
-            </a>
             <a target="_blank" rel="noreferrer" href="https://www.deviantart.com/santiari1">
                 <img src="https://cdn-icons-png.flaticon.com/512/3669/3669921.png" alt="DeviantArtIcon"></img>
             </a>
+            <a target="_blank" rel="noreferrer" href="https://twitter.com/5ar_santi">
+                <img src="https://cdn-icons-png.flaticon.com/512/1384/1384017.png" alt="TwitterIcon"></img>
+            </a>
             <a target="_blank" rel="noreferrer" href="https://www.youtube.com/@5ar_santi-illustrations140">
-            <img src="https://cdn-icons-png.flaticon.com/512/3669/3669688.png" alt="YouTubeIcon"></img>
+                <img src="https://cdn-icons-png.flaticon.com/512/3669/3669688.png" alt="YouTubeIcon"></img>
             </a>
         </div>
     );
