52b8b025849113db79b9648857b78cb29ee15097
5arSanti <santiari05@hotmail.com>
2023-06-16 13:59:59 -0500

    Bug fixes

--- src/Pages/components/NavBarResponsive/index.jsx
+++ src/Pages/components/NavBarResponsive/index.jsx
@@ -7,7 +7,7 @@ import { PortfolioContext } from "../../../Context";
 import "./styles.css"
 
 const NavBarResponsive = () => {
-    const context = React.useContext(PortfolioContext); 
+    const context = React.useContext(PortfolioContext);
 
     return(
             <aside className={`${context.isNavbarMenuOpen ? "menu-open" : ""} z-10`} 
