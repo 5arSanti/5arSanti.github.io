0a00bb661c7278f50f0abc85ad9bcb5c33662ce6
5arSanti <santiari05@hotmail.com>
2023-06-19 12:25:37 -0500

    Redesign in the skills section and the corresponding data of each one was added.

--- src/Pages/components/Skills/index.jsx
+++ src/Pages/components/Skills/index.jsx
@@ -19,6 +19,7 @@ const Skills = () => {
                     />
                 ))}
             </div>
+            <div className="gradient-skills"></div>
         </section>
     );
 }
--- src/utils/skillsList.js
+++ src/utils/skillsList.js
@@ -1,38 +1,77 @@
+const date = new Date();
+const year = date.getFullYear();
+
+//Imagenes
+const kritaImg = "https://topbestalternative.com/wp-content/uploads/2019/09/krita-logo-600x600.png";
+const illustratorImg = "https://www.imagensempng.com.br/wp-content/uploads/2020/12/illustrator.png";
+const htmlImg = "https://cdn-icons-png.flaticon.com/512/174/174854.png";
+const cssImg = "https://cdn-icons-png.flaticon.com/512/732/732190.png";
+const javaScriptImg = "https://cdn-icons-png.flaticon.com/512/5968/5968292.png";
+const reactImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png";
+const tailwindImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png";
+const pythonImg = "https://cdn-icons-png.flaticon.com/512/5968/5968350.png";
+const cImg = "https://cdn-icons-png.flaticon.com/512/6132/6132222.png";
+const javaImg = "https://cdn-icons-png.flaticon.com/512/5968/5968282.png";
+
 export const skills = [
     {
         id: 1,
         name: "Krita",
-        experience: 2,
-        image: "https://topbestalternative.com/wp-content/uploads/2019/09/krita-logo-600x600.png",
+        experience: year - 2019,
+        image: kritaImg,
     },
     {
         id: 2,
-        name: "JavaScript",
-        experience: 2,
-        image: "https://topbestalternative.com/wp-content/uploads/2019/09/krita-logo-600x600.png",
+        name: "Illustrator",
+        experience: year - 2021,
+        image: illustratorImg,
     },
     {
-        id: 2,
-        name: "JavaScript",
-        experience: 2,
-        image: "https://topbestalternative.com/wp-content/uploads/2019/09/krita-logo-600x600.png",
+        id: 3,
+        name: "HTML",
+        experience: year - 2020,
+        image: htmlImg,
     },
     {
-        id: 2,
-        name: "JavaScript",
-        experience: 2,
-        image: "https://topbestalternative.com/wp-content/uploads/2019/09/krita-logo-600x600.png",
+        id: 4,
+        name: "CSS",
+        experience: year - 2020,
+        image: cssImg,
     },
     {
-        id: 2,
+        id: 5,
         name: "JavaScript",
-        experience: 2,
-        image: "https://topbestalternative.com/wp-content/uploads/2019/09/krita-logo-600x600.png",
+        experience: year - 2020,
+        image: javaScriptImg,
     },
     {
-        id: 2,
-        name: "JavaScript",
-        experience: 2,
-        image: "https://topbestalternative.com/wp-content/uploads/2019/09/krita-logo-600x600.png",
+        id: 6,
+        name: "ReactJS",
+        experience: year - 2022,
+        image: reactImg,
+    },
+    {
+        id: 7,
+        name: "TailwindCSS",
+        experience: year - 2022,
+        image: tailwindImg,
+    },
+    {
+        id: 8,
+        name: "Python",
+        experience: year - 2020,
+        image: pythonImg,
+    },
+    {
+        id: 9,
+        name: "C++",
+        experience: year - 2022,
+        image: cImg,
+    },
+    {
+        id: 10,
+        name: "Java",
+        experience: year - 2022,
+        image: javaImg,
     },
 ]
