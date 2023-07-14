d77ec59c85b7029f845fe65d74627efb3cdfed61
5arSanti <santiari05@hotmail.com>
2023-07-14 18:17:26 -0500

    Fixed all anchor bugs resulting from the change from BrowserRouter to HashRoutes.

--- src/Pages/components/NavOptions/index.jsx
+++ src/Pages/components/NavOptions/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { HiChevronRight } from "react-icons/hi2";
 import { DownloadCV } from "../DownloadCV";
 import { PortfolioContext } from "../../../Context";
+import { HashLink } from "react-router-hash-link";
 
 
 const NavOptions = () => {
@@ -12,26 +13,26 @@ const NavOptions = () => {
         if (context.isHomeActive){
             return(
                 <div className="navbar-right flex gap-6 text-sm">
-                    <a href="/" className="animacion flex gap-1 items-center cursor-pointer">
+                    <HashLink to="/#home" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Home</p>
                         <HiChevronRight/>
-                    </a>
-                    <a href="#about-me" className="animacion flex gap-1 items-center cursor-pointer">
+                    </HashLink>
+                    <HashLink to="/#about-me" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Sobre Mi</p>
                         <HiChevronRight/>
-                    </a>
-                    <a href="#projects" className="animacion flex gap-1 items-center cursor-pointer">
+                    </HashLink>
+                    <HashLink to="/#projects" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Proyectos</p>
                         <HiChevronRight/>
-                    </a>
-                    <a href="#skills" className="animacion flex gap-1 items-center cursor-pointer">
+                    </HashLink>
+                    <HashLink to="/#skills" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Habilidades</p>
                         <HiChevronRight/>
-                    </a>
-                    <a href="#contact-me" className="animacion flex gap-1 items-center cursor-pointer">
+                    </HashLink>
+                    <HashLink to="/#contact-me" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Contacto</p>
                         <HiChevronRight/>
-                    </a>
+                    </HashLink>
                     <div className="nav-button flex gap-1 items-center cursor-pointer">
                         <DownloadCV/>
                     </div>
@@ -41,18 +42,18 @@ const NavOptions = () => {
         else if (context.isIllustrationsActive){
             return(
                 <div className="navbar-right flex gap-6 text-sm">
-                    <a href="#illustrations-home" className="animacion flex gap-1 items-center cursor-pointer">
+                    <HashLink to="/ilustraciones/#illustrations-home" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Home</p>
                         <HiChevronRight/>
-                    </a>
-                    <a href="#time-lapses" className="animacion flex gap-1 items-center cursor-pointer">
+                    </HashLink>
+                    <HashLink to="/ilustraciones/#time-lapses" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Time lapses</p>
                         <HiChevronRight/>
-                    </a>
-                    <a href="#illustrations" className="animacion flex gap-1 items-center cursor-pointer">
+                    </HashLink>
+                    <HashLink to="/ilustraciones/#illustrations" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Ilustraciones</p>
                         <HiChevronRight/>
-                    </a>
+                    </HashLink>
                     <div className="nav-button flex gap-1 items-center cursor-pointer">
                         <DownloadCV/>
                     </div>
@@ -62,18 +63,18 @@ const NavOptions = () => {
         else if (context.isWebPagesActive){
             return(
                 <div className="navbar-right flex gap-6 text-sm">
-                    <a href="#web-pages-home" className="animacion flex gap-1 items-center cursor-pointer">
+                    <HashLink to="/paginas-web/#web-pages-home" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Home</p>
                         <HiChevronRight/>
-                    </a>
-                    <a href="#web-pages" className="animacion flex gap-1 items-center cursor-pointer">
+                    </HashLink>
+                    <HashLink to="/paginas-web/#web-pages" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Paginas web</p>
                         <HiChevronRight/>
-                    </a>
-                    <a href="#front-mentor" className="animacion flex gap-1 items-center cursor-pointer">
+                    </HashLink>
+                    <HashLink to="/paginas-web/#front-mentor" className="animacion flex gap-1 items-center cursor-pointer">
                         <p>Frontend Mentor</p>
                         <HiChevronRight/>
-                    </a>
+                    </HashLink>
                     <div className="nav-button flex gap-1 items-center cursor-pointer">
                         <DownloadCV/>
                     </div>
--- src/Pages/components/TopButton/index.jsx
+++ src/Pages/components/TopButton/index.jsx
@@ -1,11 +1,12 @@
 import { HiChevronDoubleUp } from "react-icons/hi2";
 import "./styles.css";
+import { HashLink } from "react-router-hash-link";
 
 const TopButton = () => {
     return(
-        <a href="#top" aria-label="Top" className="top-button animacion-top-button">
+        <HashLink to="#top" aria-label="Top" className="top-button animacion-top-button">
             <HiChevronDoubleUp/>
-        </a>
+        </HashLink>
     );
 }
 export { TopButton };
